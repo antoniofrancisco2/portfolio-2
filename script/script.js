e02f98ed7dca1e70fe33c94f7a717abf2ea4f8ef
Antônio Francisco Da Silva Lopes <92190449+antoniofrancisco2@users.noreply.github.com>
2026-04-02 10:08:58 -0300

    up

--- script/script.js
+++ script/script.js
@@ -1,21 +1,59 @@
+console.log("JS carregou");
+
+ScrollReveal().reveal('.hero', {
+  distance: '50px',
+  duration: 800,
+  origin: 'bottom',
+  reset: false
+});
+
+ScrollReveal().reveal('#sobre', {
+  distance: '50px',
+  duration: 800,
+  origin: 'bottom',
+  delay: 200
+});
+
+ScrollReveal().reveal('#habilidades .skill-card', {
+  interval: 200
+});
+
+ScrollReveal().reveal('#projetos .projeto', {
+  interval: 200
+});
+
+ScrollReveal().reveal('#contato', {
+  delay: 200
+});
+
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+
+  if (window.scrollY > 50) {
+    header.classList.add("scroll");
+  } else {
+    header.classList.remove("scroll");
+  }
+});
+
 const projetos = [
   {
-    nome: "Cafeteria",
-    descricao: "cafeteria dos sonhos",
-    imagem: "imagens/download.jpg",
-    link: "#"
+    nome: "CoffeeTime",
+    descricao: "CoffeeTime é um site moderno para uma cafeteria, com cardápio interativo, informações sobre o local e design responsivo, proporcionando uma experiência fluida e acolhedora em qualquer dispositivo.",
+    imagem: "imagens/projeto-1.png",
+    link: "https://antoniofrancisco2.github.io/projeto-cafeteria/"
   },
   {
-    nome: "MixOn",
-    descricao: "E-commerce completo.",
-    imagem: "imagens/mixon.png",
-    link: "#"
+    nome: "Cine Flix",
+    descricao: "Site responsivo que simula um aplicativo de filmes e séries, desenvolvido com tecnologias de front-end. Possui um design moderno e experiência de usuário otimizada, focando em navegação intuitiva e visual atrativo.",
+    imagem: "imagens/cineflix-websit.png",
+    link: "https://antoniofrancisco2.github.io/movie-website/"
   },
   {
-    nome: "InfoPrime",
-    descricao: "Assistência técnica.",
-    imagem: "imagens/infoprime.png",
-    link: "#"
+    nome: "Projeto NLW Connect",
+    descricao: "O projeto é um site responsivo de inscrição e indicação para eventos. Esse é um projeto do evento NLW Connect, um dos conteúdos disponíveis para alunos da Rocketseat.",
+    imagem: "imagens/nlw-connect-devStage.png",
+    link: "https://antoniofrancisco2.github.io/nlw-connect-devstage/"
   }
 ];
 
